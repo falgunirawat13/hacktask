bb87e9ff8ed5bdd3d7606e126824e354c8f4973e
falguni rawat <rawatfalguni13@gmail.com>
2024-09-05 19:48:40 +0530

    frontend done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
